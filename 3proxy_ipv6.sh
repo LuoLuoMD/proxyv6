08fbbdb29bd4da4080418ce2dcb46a104714c148
LuoLuoMD <115504416+LuoLuoMD@users.noreply.github.com>
2025-08-07 17:13:08 -0500

    proxyv6

--- 3proxy_ipv6.sh
+++ 3proxy_ipv6.sh
@@ -1,68 +1,122 @@
-#!/bin/bash
+#!/bin/sh
+random() {
+	tr </dev/urandom -dc A-Za-z0-9 | head -c5
+	echo
+}
 
-# Biến cấu hình
-PROXY_USER="proxyuser"
-PROXY_PASS="proxypass"
-COUNT=100
-IPV6_NET="2001:db8:100::"
-IPV6_INTERFACE=$(ip -6 route show default | awk '{print $5}' | head -n1)
+array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+gen64() {
+	ip64() {
+		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+	}
+	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+}
 
-# Cài đặt các gói cần thiết
-yum install -y gcc git make wget net-tools
+install_3proxy() {
+    echo "installing 3proxy"
+    URL="https://raw.githubusercontent.com/quayvlog/quayvlog/main/3proxy-3proxy-0.8.6.tar.gz"
+    wget -qO- $URL | bsdtar -xvf-
+    cd 3proxy-3proxy-0.8.6 || exit 1
+    make -f Makefile.Linux || exit 1
+    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    cp ./src/3proxy /usr/local/etc/3proxy/bin/ || cp ./bin/3proxy /usr/local/etc/3proxy/bin/
+    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+    chmod +x /etc/init.d/3proxy
+    chkconfig 3proxy on
+    cd $WORKDIR || exit 1
+}
 
-# Tải 3proxy 0.9.5 nếu chưa có
-cd /root
-if [ ! -d "3proxy-0.9.5" ]; then
-  wget https://github.com/z3APA3A/3proxy/archive/refs/tags/0.9.5.tar.gz
-  tar -xvzf 0.9.5.tar.gz
-fi
-
-# Build 3proxy
-cd /root/3proxy-0.9.5
-make -f Makefile.Linux
-
-# Tạo thư mục chứa proxy
-mkdir -p /home/3proxy
-cd /home/3proxy
-
-# Lấy địa chỉ IPv4
-IP4=$(curl -s ipv4.icanhazip.com)
-
-# Tạo danh sách proxy + cấu hình
-echo > proxy.txt
-for ((i=1;i<=$COUNT;i++)); do
-    LAST_HEX=$(printf "%x\n" $i)
-    IP6="$IPV6_NET$LAST_HEX"
-    echo "$PROXY_USER:$PROXY_PASS:$IP4:100$i:$IP6" >> proxy.txt
-done
-
-# Tạo cấu hình 3proxy
-cat <<EOF > 3proxy.cfg
+gen_3proxy() {
+    cat <<EOF
 daemon
 maxconn 1000
-nserver 8.8.8.8
 nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
-users $PROXY_USER:CL:$PROXY_PASS
+setgid 65535
+setuid 65535
+flush
 auth strong
+
+users (awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
+
+$(awk -F "/" '{print "auth strong\n" \
+"allow " $1 "\n" \
+"proxy -6 -n -a -p" $4 " -i" $3 " -e" $5 "\n" \
+"flush\n"}' ${WORKDATA})
+EOF
+}
+
+gen_proxy_file_for_user() {
+    cat >proxy.txt <<EOF
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
+EOF
+}
+
+upload_proxy() {
+    local PASS=$(random)
+    zip --password $PASS proxy.zip proxy.txt
+    URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
+
+    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
+    echo "Download zip archive from: ${URL}"
+    echo "Password: ${PASS}"
+
+}
+
+gen_data() {
+    seq $FIRST_PORT $LAST_PORT | while read port; do
+        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
+    done
+}
+
+gen_iptables() {
+    cat <<EOF
+    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+EOF
+}
+
+gen_ifconfig() {
+    cat <<EOF
+$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
+EOF
+}
+
+echo "installing apps"
+yum -y install gcc net-tools bsdtar zip >/dev/null
+
+WORKDIR="/home/proxy-installer"
+WORKDATA="${WORKDIR}/data.txt"
+mkdir -p $WORKDIR && cd $WORKDIR
+
+install_3proxy
+
+IP4=$(curl -4 -s icanhazip.com)
+IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
+
+echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
+
+echo "How many proxy do you want to create? Example 500"
+read COUNT
+
+FIRST_PORT=10000
+LAST_PORT=$(($FIRST_PORT + $COUNT))
+
+gen_data >$WORKDATA
+gen_iptables >$WORKDIR/boot_iptables.sh
+gen_ifconfig >$WORKDIR/boot_ifconfig.sh
+chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
+
+gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
+
+cat >>/etc/rc.local <<EOF
+bash ${WORKDIR}/boot_iptables.sh
+bash ${WORKDIR}/boot_ifconfig.sh
+ulimit -n 10048
+service 3proxy start
 EOF
 
-for ((i=1;i<=$COUNT;i++)); do
-    LAST_HEX=$(printf "%x\n" $i)
-    echo "proxy -6 -n -a -p100$i -i$IP4 -e$IPV6_NET$LAST_HEX" >> 3proxy.cfg
-done
+bash /etc/rc.local
 
-# Tạo interface IPv6
-for ((i=1;i<=$COUNT;i++)); do
-    LAST_HEX=$(printf "%x\n" $i)
-    ip -6 addr add "$IPV6_NET$LAST_HEX"/64 dev "$IPV6_INTERFACE"
-done
+gen_proxy_file_for_user
 
-# Copy file chạy 3proxy
-cp /root/3proxy-0.9.5/src/3proxy /usr/local/bin/3proxy
-chmod +x /usr/local/bin/3proxy
-
-# Chạy 3proxy
-/usr/local/bin/3proxy /home/3proxy/3proxy.cfg
-
-echo "✅ Proxy đã khởi động. File proxy nằm tại /home/3proxy/proxy.txt"
+upload_proxy
